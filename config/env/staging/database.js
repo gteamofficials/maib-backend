46425b99e12ec415a9ff77666d495f776a47ad85
hilmiwicak <hilmiwicaksonop@gmail.com>
2022-05-17 19:19:48 +0700

    cicd: update node env var

--- config/env/staging/database.js
+++ config/env/staging/database.js
@@ -1,14 +1,14 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('STAGING_HOST'),
-      port: env('STAGING_PORT'),
-      database: env('STAGING_DATABASE'),
-      user: env('STAGING_USER'),
-      password: env('STAGING_PASSWORD'),
+      host: env("STAGING_HOST"),
+      port: env("STAGING_PORT"),
+      database: env("STAGING_DATABASE"),
+      user: env("STAGING_USER"),
+      password: env("STAGING_PASSWORD"),
       ssl: {
-        rejectUnauthorized: false
+        rejectUnauthorized: false,
       },
     },
     debug: false,
