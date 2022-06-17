ad32cd739ff958616562cd4d26653920a2b76466
andranugraha <73644729+andranugraha@users.noreply.github.com>
2022-06-17 16:41:10 +0700

    fix prettier error

--- src/api/information/services/information.js
+++ src/api/information/services/information.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 /**
  * information service.
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService('api::information.information');
+module.exports = createCoreService("api::information.information");
